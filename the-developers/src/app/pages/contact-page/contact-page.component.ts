3e79a03663bef35e2aba6fe6c3d0ebf03cdd2ead
bervalim <bernardogvalim@gmail.com>
2024-03-22 03:23:13 -0300

    feat/contact-section-structured

--- the-developers/src/app/pages/contact-page/contact-page.component.ts
+++ the-developers/src/app/pages/contact-page/contact-page.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
+import { ContactSectionComponent } from '../../components/sections/contact-section/contact-section.component';
+import { ContactInformationComponent } from '../../components/sections/contact-information/contact-information.component';
 
 @Component({
   selector: 'app-contact-page',
   standalone: true,
-  imports: [],
+  imports: [ContactSectionComponent, ContactInformationComponent],
   templateUrl: './contact-page.component.html',
-  styleUrl: './contact-page.component.scss'
+  styleUrl: './contact-page.component.scss',
 })
-export class ContactPageComponent {
-
-}
+export class ContactPageComponent {}
